5c2ee50d31fa0b0e8f7738654246327e92b1d306
Pian <alifalpian157@gmail.com>
2024-06-07 10:50:22 +0700

    code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,7 @@
             <!-- Brand -->
             <a class="navbar-brand text-white" href="{{ url('/') }}">Atlantis</a>
             <!-- Toggler Button for Mobile -->
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <!-- Navigation Links -->
