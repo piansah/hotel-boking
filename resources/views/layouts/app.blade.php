82decbba00429023c703c321299d0ff505ea24b7
Pian <alifalpian157@gmail.com>
2024-06-07 10:49:20 +0700

    code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 
     <!-- External Dependencies -->
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -76,38 +76,41 @@
     <div class="background-overlay"></div>
 
     <!-- Navigation Bar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-        <!-- Brand -->
-        <a class="navbar-brand text-white" href="{{ url('/') }}">Atlantis</a>
-        <!-- Toggler Button for Mobile -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <!-- Navigation Links -->
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mr-auto">
-                <!-- Home Link -->
-                <li class="nav-item active">
-                    <a class="nav-link text-white" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <!-- Booking Link -->
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="{{ url('/rooms') }}">Room List</a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{ route('reservations.create') }}" class="btn btn-primary">Booking</a>
-                </li>
-                <!-- Statics Link -->
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="{{ route('charts.index') }}">Statics</a>
-                </li>
-                <!-- About Link -->
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="{{ route('about') }}">About</a>
-                </li>
-            </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top py-3">
+        <div class="container">
+            <!-- Brand -->
+            <a class="navbar-brand text-white" href="{{ url('/') }}">Atlantis</a>
+            <!-- Toggler Button for Mobile -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Navigation Links -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <!-- Home Link -->
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{ url('/') }}">Home</a>
+                    </li>
+                    <!-- Booking Link -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/rooms') }}">Room List</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('reservations.create') }}" >Booking</a>
+                    </li>
+                    <!-- Statics Link -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('charts.index') }}">Statics</a>
+                    </li>
+                    <!-- About Link -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('about') }}">About</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
+
     <br><br>
 
     <!-- Main Content -->
@@ -119,5 +122,8 @@
     <footer class="footer">
         <p>&copy; 2024 Hotel Atlantis. All rights reserved.</p>
     </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
